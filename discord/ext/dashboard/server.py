8193549985d10bb1492409c4417f761939dc55ab
BANanaD3V <banana@bananadev.ml>
2021-11-22 13:20:45 +0300

    Update server.py

--- discord/ext/dashboard/server.py
+++ discord/ext/dashboard/server.py
@@ -26,7 +26,7 @@ class Server:
 		request = json.dumps(request_data)
 		
 		async with aiohttp.ClientSession() as session:
-			webhook = Webhook.from_url(self.webhook_url)
+			webhook = Webhook.from_url(url=self.webhook_url, session=session)
 			await webhook.send(request)
 			
 		await asyncio.sleep(self.sleep_time)
